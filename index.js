8e5ac6188c106861eedbe6560f466fb4a50889f9
Chhorvy Om <vivi@chhorvys-imac.attlocal.net>
2022-03-23 22:41:22 -0700

    Task 2: Created Car constructor and .fill prototype

--- index.js
+++ index.js
@@ -83,9 +83,18 @@ Person.prototype.toString = function() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+Car.prototype.fill = function (gallons) {
+  this.tank = this.tank + gallons;
+};
+
+const newCar = new Car('Toyota', 50);
+console.log(newCar.fill(30));
 
 
 /*
